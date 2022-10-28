487bbc36b192e475c5ba170ace2c0c6166a38794
Dmitry Kuchukov <kudim84@gmail.com>
2022-10-28 18:54:47 +0300

    fix: delete tag selectors  CSS according to rule 3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -32,8 +32,11 @@ var createNewTaskElement = function (taskString) {
   var deleteButton = document.createElement("button");//delete button
   var deleteButtonImg = document.createElement("img");//delete button image
 
+
+  listItem.className = "list-item"
+
   label.innerText = taskString;
-  label.className = 'task';
+  label.className = 'task-label';
 
   //Each elements, needs appending
   checkBox.type = "checkbox";
